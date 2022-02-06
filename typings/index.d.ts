db749907b43630aa450802bc00687047a7e26a13
李铭昕 <715557344@qq.com>
2022-02-06 15:03:55 +0800

    代码优化

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,8 @@
 import { TUIComponent } from "./src";
 import { App, Plugin } from "vue";
 
-export declare class HelloWorld extends TUIComponent implements Plugin {
+export declare class TUIComponentPlugin extends TUIComponent implements Plugin {
   static install(app: App): void;
 }
+
+export declare class HelloWorld extends TUIComponentPlugin {}
