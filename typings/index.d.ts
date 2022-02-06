bc2e239e4d56bffcb7b63c3d4ecd1e2478ad50db
李铭昕 <715557344@qq.com>
2022-02-06 14:54:41 +0800

    Update

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,6 @@
 import { TUIComponent } from "./src";
+import { App, Plugin } from "vue";
 
-export declare class HelloWorld extends TUIComponent {}
+export declare class HelloWorld extends TUIComponent implements Plugin {
+  static install(app: App): void;
+}
